8ae8da963952e42bc20fcca0c86d8700190a1fc7
mirandadiscos <mirandaguedes.victor@gmail.com>
2022-01-28 15:57:22 -0300

    Adicionando função de sessão ao logar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
+  delete "logout", to: "sessions#destroy"
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   root to: "main#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
