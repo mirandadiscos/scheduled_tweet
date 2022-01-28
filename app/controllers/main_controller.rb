4ecda611e48eed9ac19b7fab1a1811ca8810fa60
mirandadiscos <mirandaguedes.victor@gmail.com>
2022-01-28 11:38:08 -0300

    SESSION NA MAIN

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,9 @@
 class MainController < ApplicationController
     def index
         if session[:user_id]
-            @user = User.find(session[:user_id])
+            @user = User.find_by_id(session[:user_id])
+        else
+
         end
     end
 end
