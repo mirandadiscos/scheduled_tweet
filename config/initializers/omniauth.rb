a56abd55cc7fdf254296c9ba4b853fb77a706a08
Victor Miranda <mirandaguedes.victor@gmail.com>
2022-02-10 16:40:27 -0300

    POST Omniauth salvando dados no current.user

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret]
+      {
+      :secure_image_url => 'true',
+      :image_size => 'original',
+      :authorize_params => {
+        :force_login => 'true',
+        :lang => 'pt'
+      }
+    }
 end
