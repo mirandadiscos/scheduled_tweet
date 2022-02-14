5d37cb61b7fe21334a05558427c00fe4881d629b
Victor Miranda <mirandaguedes.victor@gmail.com>
2022-02-14 14:31:28 -0300

    Update Rails 6.1.4.6 and destroy twitter acc

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -4,4 +4,10 @@ class TwitterAccountsController < ApplicationController
         @twitter_accounts = Current.user.twitter_accounts
     end
 
+    def destroy
+        @twitter_account = Current.user.twitter_accounts.find(params[:id])
+        @twitter_account.destroy
+        redirect_to twitter_accounts_path ,notice: "Successfully disconnect from twitter account"
+    end
+
 end
