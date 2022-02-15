b75f4787725667a3b1eb6e82b364aaacad6c2614
Victor Miranda <mirandaguedes.victor@gmail.com>
2022-02-15 16:59:28 -0300

    Sidekiq background job

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -2,6 +2,7 @@ class TweetJob < ApplicationJob
   queue_as :default
 
   def perform(tweet)
+    binding.irb
     return if tweet.published?
     return if tweet.publish_at > Time.current
     tweet.publish_to_twitter!
